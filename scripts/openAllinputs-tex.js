b5a43fc0192294418a8e8eaf36fa9f801adc3121
Henrik Skov Midtiby <hemi@kbm.sdu.dk>
2011-11-04 11:41:51 +0100

    Starting to clean up code.

--- scripts/openAllinputs-tex.js
+++ scripts/openAllinputs-tex.js
@@ -35,152 +35,169 @@
 
  */
 
-////////////// Premable Begin \\\\\\\\\\\\\
 
-var inDocument = false;
-var explicitSelection = false;
-var checkForIncludeOnly = false;
-var buildIncludeOnly='';
-var followThese = new Array();
-var includeList = new Array();
-var includeFound = '';
+function showObject(inputObject)
+{
+	var tempText = "";
+	for(prop in inputObject){
+		tempText += prop + " -> " + inputObject[prop] + "\n";
+	}
+	TW.information(null, "Hej", tempText);
+}
+function mainfunction()
+{
+	////////////// Premable Begin \\\\\\\\\\\\\
 
-var currentFileName  =  TW.target.fileName;
-var currentDirectoryLastDelim  =  currentFileName.lastIndexOf('/');
-var  currentDirectory  = currentFileName.substr(0,currentDirectoryLastDelim +1);
+	var inDocument = false;
+	var explicitSelection = false;
+	var checkForIncludeOnly = false;
+	var buildIncludeOnly='';
+	var followThese = new Array();
+	var includeList = new Array();
+	var includeFound = '';
 
-var txt = TW.target.selection;
-if (txt == "")
-{ 
-	var selStart =TW.target.selectionStart;
-	TW.target.balanceDelimiters(); // check if cursor is in an input or include 
-	txt = TW.target.selection;
-	TW.target.selectRange(selStart); /* protect document form accidental alteration of script selected area
-											reposition cursor to original position*/
+	var currentFileName  =  TW.target.fileName;
+	var currentDirectoryLastDelim  =  currentFileName.lastIndexOf('/');
+	var  currentDirectory  = currentFileName.substr(0,currentDirectoryLastDelim +1);
 
-	if (txt != "")
-	{
-		if ( (txt.substr(0,1) == "{") & (txt.indexOf('.tex') > -1) )
+	var txt = TW.target.selection;
+	if (txt == "")
+	{ 
+		var selStart =TW.target.selectionStart;
+		TW.target.balanceDelimiters(); // check if cursor is in an input or include 
+		txt = TW.target.selection;
+		TW.target.selectRange(selStart); /* protect document form accidental alteration of script selected area
+												reposition cursor to original position*/
+
+		if (txt != "")
 		{
-			txt = '\\input' + txt; // make ready for processing as below to open one file where cursor was between braces {my-file.tex}
+			if ( (txt.substr(0,1) == "{") & (txt.indexOf('.tex') > -1) )
+			{
+				txt = '\\input' + txt; // make ready for processing as below to open one file where cursor was between braces {my-file.tex}
+				inDocument = true;
+				explicitSelection = true;
+			}
+		}		
+		else     
+		{
+			txt = TW.target.text;
+		}
+	}// if (txt == "")
+	else  // User has explicitly chosen to open these, no matter where they are
+	{
 			inDocument = true;
 			explicitSelection = true;
-		}
-	}		
-	else     
+	} 
+	var alLines = txt.split("\n");
+
+	////////////// Premable End \\\\\\\\\\\\\
+
+
+	if (explicitSelection == false) // whole master document is being processed
 	{
-		txt = TW.target.text;
-	}
-}// if (txt == "")
-else  // User has explicitly chosen to open these, no matter where they are
-{
-		inDocument = true;
-		explicitSelection = true;
-} 
-var alLines = txt.split("\n");
-
-////////////// Premable End \\\\\\\\\\\\\
-
-
-if (explicitSelection == false) // whole master document is being processed
-{
-	var startIncludeOnly = txt.indexOf('\\includeonly{'); // there is at least one in document
-	if (startIncludeOnly > -1)
-	{
-		// weed out commented out %\includeonly{}-s
-		for (check in alLines)
+		var startIncludeOnly = txt.indexOf('\\includeonly{'); // there is at least one in document
+		if (startIncludeOnly > -1)
 		{
-			if (alLines[check].indexOf('\\includeonly{') == 0) // not commented out should only be one \includeonly{ at this position in premable
+			// weed out commented out %\includeonly{}-s
+			for (check in alLines)
 			{
-				includeFound = alLines[check];
-				checkForIncludeOnly = true ;
-				buildIncludeOnly = includeFound.substr(13, includeFound.length - 12);	
-				endIncludeOnly = buildIncludeOnly.indexOf("}");	
-				buildIncludeOnly = buildIncludeOnly.substr(0, endIncludeOnly);
-				includeList = buildIncludeOnly.split();
-				break; // should only be one active  \includeonly in preamble
-			}
-		} // /End. 	for (check in alLines)
-	} // /End. if (startIncludeOnly > -1)
-}	 // /End. (explicitSelection == false)
-
-// TW.warning(null,"Build Include Only:", buildIncludeOnly);		 
-
-for (line in alLines)
-{
-	if (alLines[line].substr(0,1) == '%'){continue;} // ignore commented lines
-
-	thisLine = alLines[line].toLowerCase();	 
-
-	if (thisLine.indexOf('\\begin{document}') >-1) {inDocument = true;}   // avoid preamble references
-	if ( (explicitSelection == false) & (thisLine.indexOf('\\end{document}')  >-1) ) {inDocument = false;}  // avoid user notes below document proper, but explicitSelection test can allow a user selection that crosses over the \end{document}.
-
-	// ADVICE: We are looking for only one \input{} per line	
-	if ( (inDocument == true) & ((thisLine.indexOf('\\input{')>-1) | (thisLine.indexOf('\\include{')>-1) )& (thisLine.indexOf('.tex}')>-1) )
-	{
-		var doingInclude = false;
-
-		if  (thisLine.indexOf('\\input{')>-1)
-		{
-			beginInput = thisLine.indexOf('\\input{') + 7;
-		}
-		else
-		{
-			beginInput = thisLine.indexOf('\\include{') + 9;
-			doingInclude = true;
-		}		 
-
-		endInput  = thisLine.indexOf('.tex}') + 4;
-
-		var fileName = alLines[line].substr(beginInput,(endInput - beginInput)); // use original reference to retain letter case of file name for sensative OSes ;) 
-		//TW.warning(null,"Filename Check:", fileName);
-		if (  (checkForIncludeOnly == true) & (doingInclude == true) & (buildIncludeOnly != "")  )
-		{
-			 var usable = false;
-
-			for (item in includeList)
-			{
-				if (includeList[item].indexOf(fileName) > -1 )
+				if (alLines[check].indexOf('\\includeonly{') == 0) // not commented out should only be one \includeonly{ at this position in premable
 				{
-					usable = true;
-					break;
+					includeFound = alLines[check];
+					checkForIncludeOnly = true ;
+					buildIncludeOnly = includeFound.substr(13, includeFound.length - 12);	
+					endIncludeOnly = buildIncludeOnly.indexOf("}");	
+					buildIncludeOnly = buildIncludeOnly.substr(0, endIncludeOnly);
+					includeList = buildIncludeOnly.split();
+					break; // should only be one active  \includeonly in preamble
 				}
-			}	
-			if (usable == false)					
-			{ 
-				continue; 
-			} // include{fileName} not in \includeonly{}
-		} 
+			} // /End. 	for (check in alLines)
+		} // /End. if (startIncludeOnly > -1)
+	}	 // /End. (explicitSelection == false)
 
-		fileName = currentDirectory + fileName; 
-		fileName = fileName.replace('//','/'); // remove any un-needed user added slash from input lead  
-		fileName = fileName.replace('/./','/'); // remove any un-needed user added this-directory slash from input lead 			
+	// TW.warning(null,"Build Include Only:", buildIncludeOnly);		 
 
-		// keeping TW 'opening' in a seperate clause for debugging and any future scripting devlopment
-		followThese.push(fileName); 
-	}
-}// /End. for	(line in alLines)
-
-// keeping TW 'opening' in a seperate clause for debugging and any future scripting development
-for (fileNum in followThese)
-{
-	fileName = followThese[fileNum];					 
-	// TW.warning(null,"Filename:", fileName);
-	openedDoc = TW.app.openFile(fileName);
-	if (openedDoc != null)
+	for (line in alLines)
 	{
-		openedDoc.showMinimized();
-	}
-}// /End.  for (fileNum in followThese)
+		if (alLines[line].substr(0,1) == '%'){continue;} // ignore commented lines
 
-if (followThese.length == 1)
-{
-	openedDoc.showNormal()
-} // user has requested only one file, show it
-else
-{
-	TW.warning(null,"Look Under Window Menu", "Files Are Available\nUnder the Window Menu");
+		thisLine = alLines[line].toLowerCase();	 
+
+		if (thisLine.indexOf('\\begin{document}') >-1) {inDocument = true;}   // avoid preamble references
+		if ( (explicitSelection == false) & (thisLine.indexOf('\\end{document}')  >-1) ) {inDocument = false;}  // avoid user notes below document proper, but explicitSelection test can allow a user selection that crosses over the \end{document}.
+
+		// ADVICE: We are looking for only one \input{} per line	
+		if ( (inDocument == true) & ((thisLine.indexOf('\\input{')>-1) | (thisLine.indexOf('\\include{')>-1) )& (thisLine.indexOf('.tex}')>-1) )
+		{
+			var doingInclude = false;
+
+			if  (thisLine.indexOf('\\input{')>-1)
+			{
+				beginInput = thisLine.indexOf('\\input{') + 7;
+			}
+			else
+			{
+				beginInput = thisLine.indexOf('\\include{') + 9;
+				doingInclude = true;
+			}		 
+
+			endInput  = thisLine.indexOf('.tex}') + 4;
+
+			var fileName = alLines[line].substr(beginInput,(endInput - beginInput)); // use original reference to retain letter case of file name for sensative OSes ;) 
+			//TW.warning(null,"Filename Check:", fileName);
+			if (  (checkForIncludeOnly == true) & (doingInclude == true) & (buildIncludeOnly != "")  )
+			{
+				 var usable = false;
+
+				for (item in includeList)
+				{
+					if (includeList[item].indexOf(fileName) > -1 )
+					{
+						usable = true;
+						break;
+					}
+				}	
+				if (usable == false)					
+				{ 
+					continue; 
+				} // include{fileName} not in \includeonly{}
+			} 
+
+			fileName = currentDirectory + fileName; 
+			fileName = fileName.replace('//','/'); // remove any un-needed user added slash from input lead  
+			fileName = fileName.replace('/./','/'); // remove any un-needed user added this-directory slash from input lead 			
+
+			// keeping TW 'opening' in a seperate clause for debugging and any future scripting devlopment
+			followThese.push(fileName); 
+		}
+	}// /End. for	(line in alLines)
+
+	// keeping TW 'opening' in a seperate clause for debugging and any future scripting development
+	for (fileNum in followThese)
+	{
+		fileName = followThese[fileNum];					 
+		// TW.warning(null,"Filename:", fileName);
+		openedDoc = TW.app.openFileFromScript(fileName, TW);
+		// TODO: Create a new file and open the save as dialog with a suggested filename.
+		//newfilename = TW.app.getSaveFileName(fileName);
+		//tempwindow = TW.app.newFile(newfilename);
+		if (openedDoc != null)
+		{
+			//openedDoc.showMinimized();
+		}
+	}// /End.  for (fileNum in followThese)
+
+	if (followThese.length == 1)
+	{
+		//openedDoc.showNormal()
+	} // user has requested only one file, show it
+	else
+	{
+		TW.warning(null,"Look Under Window Menu", "Files Are Available\nUnder the Window Menu");
+	}
+	null;
 }
-null;
+
+mainfunction();
 
 
