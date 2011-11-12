f199dc93693563282c3b4d62e4920b050d78dafa
Henrik Skov Midtiby <hemi@kbm.sdu.dk>
2011-11-12 20:23:18 +0100

    Extracted methods initializeParameters, addedErrorFromLine and addInfoFromLine.

--- scripts/Hooks/latexErrors.js
+++ scripts/Hooks/latexErrors.js
@@ -13,121 +13,136 @@
 
 function LatexErrorAnalyzer() {
 	var obj = {};
-	parenRE = new RegExp("[()]");
-	// Should catch filenames of the following forms:
-	// * ./abc, "./abc"
-	// * /abc, "/abc"
-	// * .\abc, ".\abc"
-	// * C:\abc, "C:\abc"
-	// * \\server\abc, "\\server\abc"
-	// Caveats: filenames with escaped " or space in the filename don't work (correctly)
-	newFileRE = new RegExp("^\\(\"?((?:\\./|/|.\\\\|[a-zA-Z]:\\\\|\\\\\\\\[^\\\" )]+\\\\)[^\" )]+)");
-	lineNumRE = new RegExp("^l\\.(\\d+)");
-	badLineRE = new RegExp("^(?:Over|Under)full \\\\hbox.*at lines (\\d+)");
-	warnLineRE = new RegExp("^(?:LaTeX|Package (?:.*)) Warning: .*");
-	warnLineNumRE = new RegExp("on input line (\\d+).");
-	errors = [];
-	warnings = [];
-	infos = [];
+	obj.initializeParameters = function()
+	{
+		this.parenRE = new RegExp("[()]");
+		// Should catch filenames of the following forms:
+		// * ./abc, "./abc"
+		// * /abc, "/abc"
+		// * .\abc, ".\abc"
+		// * C:\abc, "C:\abc"
+		// * \\server\abc, "\\server\abc"
+		// Caveats: filenames with escaped " or space in the filename don't work (correctly)
+		this.newFileRE = new RegExp("^\\(\"?((?:\\./|/|.\\\\|[a-zA-Z]:\\\\|\\\\\\\\[^\\\" )]+\\\\)[^\" )]+)");
+		this.lineNumRE = new RegExp("^l\\.(\\d+)");
+		this.badLineRE = new RegExp("^(?:Over|Under)full \\\\hbox.*at lines (\\d+)");
+		this.warnLineRE = new RegExp("^(?:LaTeX|Package (?:.*)) Warning: .*");
+		this.warnLineNumRE = new RegExp("on input line (\\d+).");
+		this.errors = [];
+		this.warnings = [];
+		this.infos = [];
+	}
+	obj.initializeParameters();
 
 	function trim (zeichenkette) {
 		return zeichenkette.replace (/^\s+/, '').replace (/\s+$/, '');
 	}
 
 	// get the text from the standard console output
-	txt = TW.target.consoleOutput;
-	lines = txt.split('\n');
+	obj.txt = TW.target.consoleOutput;
+	obj.lines = obj.txt.split('\n');
 
-	curFile = undefined;
-	filenames = [];
-	extraParens = 0;
+	obj.curFile = undefined;
+	obj.filenames = [];
+	obj.extraParens = 0;
 
-	for (i = 0; i < lines.length; ++i) {
-		line = 	lines[i];
+	obj.addErrorFromLine = function(line)
+	{
+		var error = [];
+		// record the current input file
+		error[0] = this.curFile;
+		// record the error message itself
+		error[2] = line;
+		// look ahead for the line number and record that
+		error[1] = 0;
+		while (++i < this.lines.length) {
+			line = this.lines[i];
+			if(trim(line) == '') break;
+			matched = this.lineNumRE.exec(line);
+			if (matched)
+				error[1] = matched[1];
+			error[2] += "\n" + line;
+		}
+		this.errors.push(error);
+	}
+
+	obj.addInfoFromLine = function(line)
+	{
+		var error = [];
+		error[0] = this.curFile;
+		error[1] = matched[1];
+		error[2] = line;
+		this.infos.push(error);
+	}
+
+
+	for (i = 0; i < obj.lines.length; ++i) {
+		line = 	obj.lines[i];
 
 		// check for error messages
 		if (line.match("^! ")) {
-			var error = [];
-			// record the current input file
-			error[0] = curFile;
-			// record the error message itself
-			error[2] = line;
-			// look ahead for the line number and record that
-			error[1] = 0;
-			while (++i < lines.length) {
-				line = lines[i];
-				if(trim(line) == '') break;
-				matched = lineNumRE.exec(line);
-				if (matched)
-					error[1] = matched[1];
-				error[2] += "\n" + line;
-			}
-			errors.push(error);
+			obj.addErrorFromLine(line);
 			continue;
 		}
 
 
 		// check for over- or underfull lines
-		matched = badLineRE.exec(line);
+		matched = obj.badLineRE.exec(line);
 		if (matched) {
-			var error = [];
-			error[0] = curFile;
-			error[1] = matched[1];
-			error[2] = line;
-			infos.push(error);
+			obj.addInfoFromLine(line);
 			continue;
 		}
 
 		// check for other warnings
-		matched = warnLineRE.exec(line);
+		matched = obj.warnLineRE.exec(line);
 		if (matched) {
 			var error = [];
-			error[0] = curFile;
+			error[0] = obj.curFile;
 			error[1] = "?";
 			error[2] = line;
 
-			while (++i < lines.length) {
-				line = lines[i];
+			while (++i < obj.lines.length) {
+				line = obj.lines[i];
 				if(line == '') break;
 				error[2] += "\n" + line;
 			}
-			matched = warnLineNumRE.exec(error[2].replace(/\n/, ""));
+			matched = obj.warnLineNumRE.exec(error[2].replace(/\n/, ""));
 			if (matched)
 				error[1] = matched[1];
-			warnings.push(error);
+			obj.warnings.push(error);
 			continue;
 		}
 
 		// try to track beginning/ending of input files (flaky!)
-		pos = line.search(parenRE);
+		pos = line.search(obj.parenRE);
 		while (pos >= 0) {
 			line = line.slice(pos);
 			if (line.charAt(0) == ")") {
-				if (extraParens > 0) {
-					--extraParens;
+				if (obj.extraParens > 0) {
+					--obj.extraParens;
 				}
-				else if (filenames.length > 0) {
-					curFile = filenames.pop();
+				else if (obj.filenames.length > 0) {
+					obj.curFile = obj.filenames.pop();
 				}
 				line = line.slice(1);
 			}
 			else {
-				match = newFileRE.exec(line);
+				match = obj.newFileRE.exec(line);
 				if (match) {
-					filenames.push(curFile);
-					curFile = match[1];
+					obj.filenames.push(obj.curFile);
+					obj.curFile = match[1];
 					line = line.slice(match[0].length);
-					extraParens = 0;
+					obj.extraParens = 0;
 				}
 				else {
-					++extraParens;
+					++obj.extraParens;
 					line = line.slice(1);
 				}
 			}
 			if (line == undefined) {
 				break;
 			}
-			pos = line.search(parenRE);
+			pos = line.search(obj.parenRE);
 		}
 	}
 
@@ -166,9 +181,9 @@ function LatexErrorAnalyzer() {
 
 	function suggestToDeleteAuxFilesIfSpecificErrorIsSeen()
 	{
-		for (index in errors)
+		for (index in obj.errors)
 		{
-			error = errors[index];
+			error = obj.errors[index];
 
 			if(error[2].indexOf("File ended while scanning use of") > -1)
 			{
@@ -178,20 +193,20 @@ function LatexErrorAnalyzer() {
 	}
 
 
-	suggestToDeleteAuxFilesIfSpecificErrorIsSeen(errors);
+	suggestToDeleteAuxFilesIfSpecificErrorIsSeen(obj.errors);
 
 
 	// finally, return our result (if any)
-	if (errors.length > 0 || warnings.length > 0 || infos.length > 0) {
+	if (obj.errors.length > 0 || obj.warnings.length > 0 || obj.infos.length > 0) {
 		html  = '<html><body>';
 		html += '<table border="1" cellspacing="0" cellpadding="4">';
 
-		for(i = 0; i < errors.length; ++i)
-			html += makeResultRow(errors[i], 'red');
-		for(i = 0; i < warnings.length; ++i)
-			html += makeResultRow(warnings[i], 'yellow');
-		for(i = 0; i < infos.length; ++i)
-			html += makeResultRow(infos[i], '#8080ff');
+		for(i = 0; i < obj.errors.length; ++i)
+			html += makeResultRow(obj.errors[i], 'red');
+		for(i = 0; i < obj.warnings.length; ++i)
+			html += makeResultRow(obj.warnings[i], 'yellow');
+		for(i = 0; i < obj.infos.length; ++i)
+			html += makeResultRow(obj.infos[i], '#8080ff');
 
 		html += "</table>";
 		html += "</body></html>";
