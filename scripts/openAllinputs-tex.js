fe2811f4d01e384da84354ee644cfca2d14337b9
Henrik Skov Midtiby <hemi@kbm.sdu.dk>
2011-11-15 10:10:33 +0100

    Converted script to an object oriented structure.

--- scripts/openAllinputs-tex.js
+++ scripts/openAllinputs-tex.js
@@ -45,197 +45,217 @@ function showObject(inputObject)
 	}
 	TW.information(null, "Hej", tempText);
 }
-function mainfunction()
+function OpenAllInputFiles()
 {
-	////////////// Premable Begin \\\\\\\\\\\\\
+	var obj = {};
+	obj.openInputFiles = function()
+	{
+		this.initializeVariables();
+		this.getTextToAnalyze();
+		this.detectIncludeOnlyStatements();
+		this.searchForInputStatements();
+		this.openLocatedFiles();
+		if (obj.followThese.length == 1)
+		{
+			//openedDoc.showNormal()
+		} // user has requested only one file, show it
+		else
+		{
+			TW.warning(null,"Look Under Window Menu", "Files Are Available\nUnder the Window Menu");
+		}
 
-	var inDocument = false;
-	var explicitSelection = false;
-	var checkForIncludeOnly = false;
-	var buildIncludeOnly='';
-	var followThese = new Array();
-	var includeList = new Array();
-	var includeFound = '';
+	}
 
-	var currentFileName  =  TW.target.fileName;
-	var currentDirectoryLastDelim  =  currentFileName.lastIndexOf('/');
-	var  currentDirectory  = currentFileName.substr(0,currentDirectoryLastDelim +1);
+	obj.initializeVariables = function()
+	{
+		this.inDocument = false;
+		this.explicitSelection = false;
+		this.checkForIncludeOnly = false;
+		this.buildIncludeOnly='';
+		this.followThese = new Array();
+		this.includeList = new Array();
+		this.includeFound = '';
 
-	var txt = TW.target.selection;
-	if (txt == "")
-	{ 
-		var selStart =TW.target.selectionStart;
-		TW.target.balanceDelimiters(); // check if cursor is in an input or include 
-		txt = TW.target.selection;
-		TW.target.selectRange(selStart); /* protect document form accidental alteration of script selected area
+		var currentFileName  =  TW.target.fileName;
+		var currentDirectoryLastDelim  =  currentFileName.lastIndexOf('/');
+		this.currentDirectory  = currentFileName.substr(0,currentDirectoryLastDelim +1);
+	}
+	obj.getTextToAnalyze = function()
+	{
+		this.txt = TW.target.selection;
+		if (this.txt == "")
+		{ 
+			var selStart =TW.target.selectionStart;
+			TW.target.balanceDelimiters(); // check if cursor is in an input or include 
+			this.txt = TW.target.selection;
+			TW.target.selectRange(selStart); /* protect document form accidental alteration of script selected area
 												reposition cursor to original position*/
 
-		if (txt != "")
-		{
-			if ( (txt.substr(0,1) == "{") & (txt.indexOf('.tex') > -1) )
+			if (this.txt != "")
 			{
-				txt = '\\input' + txt; // make ready for processing as below to open one file where cursor was between braces {my-file.tex}
-				inDocument = true;
-				explicitSelection = true;
+				if ( (this.txt.substr(0,1) == "{") & (this.txt.indexOf('.tex') > -1) )
+				{
+					this.txt = '\\input' + this.txt; // make ready for processing as below to open one file where cursor was between braces {my-file.tex}
+					this.inDocument = true;
+					this.explicitSelection = true;
+				}
+			}		
+			else     
+			{
+				this.txt = TW.target.text;
 			}
-		}		
-		else     
+		}// if (txt == "")
+		else  // User has explicitly chosen to open these, no matter where they are
 		{
-			txt = TW.target.text;
-		}
-	}// if (txt == "")
-	else  // User has explicitly chosen to open these, no matter where they are
-	{
-			inDocument = true;
-			explicitSelection = true;
-	} 
-	var alLines = txt.split("\n");
-
+			this.inDocument = true;
+			this.explicitSelection = true;
+		} 
+		this.alLines = this.txt.split("\n");
+	}
 	////////////// Premable End \\\\\\\\\\\\\
 
-
-	if (explicitSelection == false) // whole master document is being processed
+	obj.detectIncludeOnlyStatements = function()
 	{
-		var startIncludeOnly = txt.indexOf('\\includeonly{'); // there is at least one in document
-		if (startIncludeOnly > -1)
+		if (this.explicitSelection == false) // whole master document is being processed
 		{
-			// weed out commented out %\includeonly{}-s
-			for (check in alLines)
+			var startIncludeOnly = this.txt.indexOf('\\includeonly{'); // there is at least one in document
+			if (startIncludeOnly > -1)
 			{
-				if (alLines[check].indexOf('\\includeonly{') == 0) // not commented out should only be one \includeonly{ at this position in premable
+				// weed out commented out %\includeonly{}-s
+				for (check in this.alLines)
 				{
-					includeFound = alLines[check];
-					checkForIncludeOnly = true ;
-					buildIncludeOnly = includeFound.substr(13, includeFound.length - 12);	
-					endIncludeOnly = buildIncludeOnly.indexOf("}");	
-					buildIncludeOnly = buildIncludeOnly.substr(0, endIncludeOnly);
-					includeList = buildIncludeOnly.split();
-					break; // should only be one active  \includeonly in preamble
-				}
-			} // /End. 	for (check in alLines)
-		} // /End. if (startIncludeOnly > -1)
-	}	 // /End. (explicitSelection == false)
-
-	// TW.warning(null,"Build Include Only:", buildIncludeOnly);		 
-
-	for (line in alLines)
+					if (this.alLines[check].indexOf('\\includeonly{') == 0) // not commented out should only be one \includeonly{ at this position in premable
+					{
+						this.includeFound = this.alLines[check];
+						this.checkForIncludeOnly = true ;
+						this.buildIncludeOnly = this.includeFound.substr(13, this.includeFound.length - 12);	
+						endIncludeOnly = this.buildIncludeOnly.indexOf("}");	
+						this.buildIncludeOnly = this.buildIncludeOnly.substr(0, endIncludeOnly);
+						this.includeList = this.buildIncludeOnly.split();
+						break; // should only be one active  \includeonly in preamble
+					}
+				} // /End. 	for (check in alLines)
+			} // /End. if (startIncludeOnly > -1)
+		}	 // /End. (explicitSelection == false)
+		// TW.warning(null,"Build Include Only:", buildIncludeOnly);		 
+	}
+	obj.searchForInputStatements = function()
 	{
-		if (alLines[line].substr(0,1) == '%'){continue;} // ignore commented lines
-
-		thisLine = alLines[line].toLowerCase();	 
-
-		if (thisLine.indexOf('\\begin{document}') >-1) {inDocument = true;}   // avoid preamble references
-		if ( (explicitSelection == false) & (thisLine.indexOf('\\end{document}')  >-1) ) {inDocument = false;}  // avoid user notes below document proper, but explicitSelection test can allow a user selection that crosses over the \end{document}.
-
-		// ADVICE: We are looking for only one \input{} per line	
-		if ( (inDocument == true) & doesLineContainInputOrInclude(thisLine))
+		for (line in this.alLines)
 		{
-			var doingInclude = false;
+			if (this.alLines[line].substr(0,1) == '%'){continue;} // ignore commented lines
 
-			if  (thisLine.indexOf('\\input{')>-1)
-			{
-				beginInput = thisLine.indexOf('\\input{') + 7;
-			}
-			else
-			{
-				beginInput = thisLine.indexOf('\\include{') + 9;
-				doingInclude = true;
-			}		 
+			var thisLine = this.alLines[line].toLowerCase();	 
 
-			endInput  = thisLine.indexOf('.tex}') + 4;
+			if (thisLine.indexOf('\\begin{document}') >-1) {this.inDocument = true;}   // avoid preamble references
+			if ( (this.explicitSelection == false) & (thisLine.indexOf('\\end{document}')  >-1) ) {this.inDocument = false;}  // avoid user notes below document proper, but explicitSelection test can allow a user selection that crosses over the \end{document}.
 
-			var fileName = alLines[line].substr(beginInput,(endInput - beginInput)); // use original reference to retain letter case of file name for sensative OSes ;) 
-			//TW.warning(null,"Filename Check:", fileName);
-			if (  (checkForIncludeOnly == true) & (doingInclude == true) & (buildIncludeOnly != "")  )
+			// ADVICE: We are looking for only one \input{} per line	
+			if ( (this.inDocument == true) & this.doesLineContainInputOrInclude(thisLine))
 			{
-				if(isFileInIncludeList(fileName, includeList))
+				var doingInclude = false;
+
+				if  (thisLine.indexOf('\\input{')>-1)
 				{
-					break;
+					beginInput = thisLine.indexOf('\\input{') + 7;
 				}
 				else
 				{
-					continue;
-				}
-			} 
+					beginInput = thisLine.indexOf('\\include{') + 9;
+					doingInclude = true;
+				}		 
 
-			fileName = currentDirectory + fileName; 
-			fileName = fileName.replace('//','/'); // remove any un-needed user added slash from input lead  
-			fileName = fileName.replace('/./','/'); // remove any un-needed user added this-directory slash from input lead 			
+				endInput  = thisLine.indexOf('.tex}') + 4;
 
-			// keeping TW 'opening' in a seperate clause for debugging and any future scripting devlopment
-			followThese.push(fileName); 
-		}
-	}// /End. for	(line in alLines)
+				var fileName = this.alLines[line].substr(beginInput,(endInput - beginInput)); // use original reference to retain letter case of file name for sensative OSes ;) 
+				//TW.warning(null,"Filename Check:", fileName);
+				if (  (this.checkForIncludeOnly == true) & (doingInclude == true) & (this.buildIncludeOnly != "")  )
+				{
+					if(this.isFileInIncludeList(fileName, this.includeList))
+					{
+						break;
+					}
+					else
+					{
+						continue;
+					}
+				} 
 
-	openLocatedFiles(followThese);
+				fileName = this.currentDirectory + fileName; 
+				fileName = fileName.replace('//','/'); // remove any un-needed user added slash from input lead  
+				fileName = fileName.replace('/./','/'); // remove any un-needed user added this-directory slash from input lead 			
 
-	if (followThese.length == 1)
-	{
-		//openedDoc.showNormal()
-	} // user has requested only one file, show it
-	else
-	{
-		TW.warning(null,"Look Under Window Menu", "Files Are Available\nUnder the Window Menu");
+				// keeping TW 'opening' in a seperate clause for debugging and any future scripting devlopment
+				this.followThese.push(fileName); 
+			}
+		}// /End. for	(line in alLines)
 	}
-	null;
-}
-function doesLineContainInputOrInclude(thisLine)
-{
-	return ((thisLine.indexOf('\\input{')>-1) | (thisLine.indexOf('\\include{')>-1) )& (thisLine.indexOf('.tex}')>-1) 
-}
-function isFileInIncludeList(fileName, includeList)
-{
-	var usable = false;
+	obj.doesLineContainInputOrInclude = function(thisLine)
+	{
+		return ((thisLine.indexOf('\\input{')>-1) | (thisLine.indexOf('\\include{')>-1) )& (thisLine.indexOf('.tex}')>-1) 
+	}
+	obj.isFileInIncludeList = function(fileName, includeList)
+	{
+		var usable = false;
 
-	for (item in includeList)
-	{
-		if (includeList[item].indexOf(fileName) > -1 )
+		for (item in includeList)
 		{
-			usable = true;
-			break;
-		}
-	}	
-	if (usable == false)					
-	{ 
-		continue; 
-	} // include{fileName} not in \includeonly{}
-}
-function openLocatedFiles(followThese)
-{
-	// keeping TW 'opening' in a seperate clause for debugging and any future scripting development
-	for (fileNum in followThese)
-	{
-		fileName = followThese[fileNum];					 
-		// TODO: Detect if the file allready exists.
-		openedDoc = TW.app.openFileFromScript(fileName, TW);
-		if(openedDoc.result == null)
-		{
-			// Failed to open document, it might be because the document does not exist.
-			// Create the document.
-			var resp = TW.writeFile(fileName, "");
-			var newWindow = TW.app.openFileFromScript(fileName, TW);
-			if(newWindow.status == 0) // SystemAccess_OK
+			if (includeList[item].indexOf(fileName) > -1 )
 			{
-				copyTEXOptionsFromCurrentDocumentToNewDocument(newWindow);
+				usable = true;
+				break;
+			}
+		}	
+		if (usable == false)					
+		{ 
+			continue; 
+		} // include{fileName} not in \includeonly{}
+	}
+	obj.openLocatedFiles = function()
+	{
+		// keeping TW 'opening' in a seperate clause for debugging and any future scripting development
+		for (fileNum in this.followThese)
+		{
+			fileName = followThese[fileNum];					 
+			// TODO: Detect if the file allready exists.
+			openedDoc = TW.app.openFileFromScript(fileName, TW);
+			if(openedDoc.result == null)
+			{
+				this.createAndOpenFile(fileName);
+			}
+		}// /End.  for (fileNum in followThese)
+	}
+	obj.createAndOpenFile = function(fileName)
+	{
+		// Failed to open document, it might be because the document does not exist.
+		// Create the document.
+		var resp = TW.writeFile(fileName, "");
+		var newWindow = TW.app.openFileFromScript(fileName, TW);
+		if(newWindow.status == 0) // SystemAccess_OK
+		{
+			copyTEXOptionsFromCurrentDocumentToNewDocument(newWindow);
+		}
+	}
+	obj.copyTEXOptionsFromCurrentDocumentToNewDocument = function(newWindow)
+	{
+		// Populate with % !TEX lines from the current document.
+		var linesInCurrentDocument = TW.target.text.split("\n");
+		for(idx in linesInCurrentDocument)
+		{
+			curLine = linesInCurrentDocument[idx];
+			if(curLine.indexOf('% !TEX') > -1)
+			{
+				newWindow.result.insertText(curLine);
+				newWindow.result.insertText("\n");
 			}
 		}
-	}// /End.  for (fileNum in followThese)
-}
-function copyTEXOptionsFromCurrentDocumentToNewDocument(newWindow)
-{
-	// Populate with % !TEX lines from the current document.
-	var linesInCurrentDocument = TW.target.text.split("\n");
-	for(idx in linesInCurrentDocument)
-	{
-		curLine = linesInCurrentDocument[idx];
-		if(curLine.indexOf('% !TEX') > -1)
-		{
-			newWindow.result.insertText(curLine);
-			newWindow.result.insertText("\n");
-		}
 	}
+	return(obj);
 }
 
-mainfunction();
+
+
+filehandler = OpenAllInputFiles();
+filehandler.openInputFiles();
 
 
