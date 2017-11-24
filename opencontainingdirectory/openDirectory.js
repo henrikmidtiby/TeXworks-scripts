d38063e471a0af9a7a1f267457f4e5b294441a7a
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2017-11-24 09:20:24 +0100

    Simplifying code.

--- opencontainingdirectory/openDirectory.js
+++ opencontainingdirectory/openDirectory.js
@@ -11,42 +11,12 @@
 function openDirectory()
 {
 	var temp = {};
-	temp.fullFilename = locateContentsOfBraces();
-	temp.pathName = getPathFromFilename(temp.fullFilename);
 	temp.currentDirectory = getPathFromFilename(TW.target.fileName);
-	temp.combinedDirectory = temp.currentDirectory + "/" + temp.pathName;
-	temp.result = TW.launchFile(temp.combinedDirectory);
+	temp.result = TW.launchFile(temp.currentDirectory);
 	if(temp.result["status"] == 2) // SystemAccess_PermissionDenied
 	{
 		TW.information(null, "System access permission denied", "Cannot open the containing directory, due to insufficient permissions.\n\nEnable \"Enable scripts to run system commands.\" in the preferences menu.");
 	}
-}
-function locateContentsOfBraces()
-{
-	// Locate word ending at cursor location.
-	var wordStart = TW.target.selectionStart;
-	var wordEnd = TW.target.selectionStart;
-
-	while("[({".indexOf(TW.target.text.charAt(wordStart - 1)) === -1)
-	{
-		wordStart = wordStart - 1;
-		if(wordStart < 1)
-		{
-			break;
-		}
-	}
-	while("])}".indexOf(TW.target.text.charAt(wordEnd + 1)) === -1)
-	{
-		wordEnd = wordEnd + 1;
-		if(wordEnd >= TW.target.text.length)
-		{
-			break;
-		}
-	}
-
-	var extractedWord = TW.target.text.substr(wordStart, wordEnd - wordStart);
-
-	return extractedWord;
 }
 function getPathFromFilename(filename)
 {
@@ -76,7 +46,6 @@ function showObject(inputObject, header)
 	}
 	TW.information(null, header, tempText);
 }
-
 
 openDirectory();
 
